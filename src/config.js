139b94c2bdf27cb8771f6cbfa62b44b7bb46b116
TheTaiMan <12tayenm2@gmail.com>
2024-11-23 22:47:13 -0600

    Better

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '.env') })
-
-// In your config.js
-module.exports = {
-  TOKEN: process.env.DISCORD_TOKEN,
-  INTENTS: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
-}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 const { Client, GatewayIntentBits } = require('discord.js')
-const fs = require('fs')
 const path = require('path')
-const config = require('./config')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-// Create a new Discord client instance
+// Create Discord client with specified intents
 const client = new Client({
-  intents: config.INTENTS.map((intent) => GatewayIntentBits[intent]),
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 })
 
-// Load events
-const eventsPath = path.join(__dirname, 'events') // Construct the path to the events directory
-const eventFiles = fs // Read the files in the events directory
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith('.js')) // Filter the files to only include those ending with '.js'
+// Event Listeners
+const { execute: handleReady } = require('./events/ready')
+const { execute: handleInteraction } = require('./events/interactionCreate')
+const { execute: handleMessage } = require('./events/messageCreate')
 
-// Loop through the event files
-for (const file of eventFiles) {
-  const event = require(path.join(eventsPath, file)) // Require the event file
-  if (event.once) {
-    // If the event should only be triggered once, register it with client.once()
-    client.once(event.name, (...args) => event.execute(...args))
-  } else {
-    // Otherwise, register it with client.on() to trigger it on every event (this is an event listener)
-    client.on(event.name, (...args) => event.execute(...args))
-  }
-}
+// Register event handlers
+client.on('ready', handleReady)
+client.on('interactionCreate', handleInteraction)
+client.on('messageCreate', handleMessage)
 
-// Log in to Discord with your bot's token
-client.login(config.TOKEN)
+// Start the bot
+client.login(process.env.DISCORD_TOKEN)
