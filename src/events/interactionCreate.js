31822a70e889cd53351ccb897eeafc9184a32aa6
TheTaiMan <12tayenm2@gmail.com>
2024-11-26 21:18:41 -0600

    Added Handle On Boarding to its own file

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -8,48 +8,10 @@ const {
   handleSelectMenuInteraction,
 } = require('../utils/questionHandler')
 
+const { handleOnboarding } = require('./interactions/handleOnboarding')
+
 const { ButtonBuilder, ActionRowBuilder } = require('discord.js')
 
-// Handles the on boarding button press
-const handleOnboarding = async (interaction) => {
-  try {
-    // Check if a channel already exists for this user
-    const existingChannel = interaction.guild.channels.cache.find(
-      (channel) => channel.name === `onboarding-${interaction.user.id}`
-    )
-
-    if (existingChannel) {
-      // If channel exists, direct them to it
-      await interaction.reply({
-        content: `You already have an ongoing verification process. Please check ${existingChannel} to complete your verification.`,
-        ephemeral: true,
-      })
-      return
-    }
-
-    // If no channel exists, create a new one
-    const channel = await createPrivateChannel(interaction)
-    const userData = UserManager.createUser(interaction.user.id, channel.id)
-
-    // Welcome Message
-    await channel.send({
-      content:
-        'Welcome! We want to confirm that you are a university student for you to be a member and have full access to the server.',
-    })
-
-    await sendNextQuestion(channel, userData)
-    await interaction.reply({
-      content: `Please check ${channel} to complete your verification.`,
-      ephemeral: true,
-    })
-  } catch (error) {
-    console.error('Channel creation error:', error)
-    await interaction.reply({
-      content: 'Sorry, there was an error creating your private channel.',
-      ephemeral: true,
-    })
-  }
-}
 
 // Handles modal button press
 const handleForm = async (interaction) => {
---
+++ src/events/interactions/handleOnboarding.js
@@ -0,0 +1,46 @@
+const UserManager = require('../../utils/UserManager')
+const { createPrivateChannel } = require('../../utils/channelManager')
+const { sendNextQuestion } = require('../../utils/questionHandler')
+
+// Handles the On Boarding Button press 
+const handleOnboarding = async (interaction) => {
+  try {
+    // Check if a channel already exists for this user
+    const existingChannel = interaction.guild.channels.cache.find(
+      (channel) => channel.name === `onboarding-${interaction.user.id}`
+    )
+
+    if (existingChannel) {
+      // If channel exists, direct them to it
+      await interaction.reply({
+        content: `You already have an ongoing verification process. Please check ${existingChannel} to complete your verification.`,
+        ephemeral: true,
+      })
+      return
+    }
+
+    // If no channel exists, create a new one
+    const channel = await createPrivateChannel(interaction)
+    const userData = UserManager.createUser(interaction.user.id, channel.id)
+
+    // Welcome Message
+    await channel.send({
+      content:
+        'Welcome! We want to confirm that you are a university student for you to be a member and have full access to the server.',
+    })
+
+    await sendNextQuestion(channel, userData)
+    await interaction.reply({
+      content: `Please check ${channel} to complete your verification.`,
+      ephemeral: true,
+    })
+  } catch (error) {
+    console.error('Channel creation error:', error)
+    await interaction.reply({
+      content: 'Sorry, there was an error creating your private channel.',
+      ephemeral: true,
+    })
+  }
+}
+
+module.exports = { handleOnboarding }
