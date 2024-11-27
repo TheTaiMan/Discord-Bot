1edf667aeaa632f09da2cdb18816cde3419855a1
TheTaiMan <12tayenm2@gmail.com>
2024-11-26 22:09:57 -0600

    It worked

--- src/events/interactions/handleModalSubmission.js
+++ src/events/interactions/handleModalSubmission.js
@@ -1,11 +1,15 @@
 const { showSummary, sendNextQuestion } = require('../../utils/questionHandler')
 const UserManager = require('../../utils/UserManager')
+const { ActionRowBuilder } = require('discord.js')
+const { questions } = require('../../questions')
 
 const handleModalSubmission = async (interaction) => {
   const questionId = interaction.customId.replace('-modal', '')
   const response = interaction.fields.getTextInputValue(
     `${questionId}-modal-input`
   )
+
+  const question = questions.find((q) => q.id === questionId)
 
   const userData = UserManager.updateUserResponse(
     interaction.user.id,
@@ -17,12 +21,39 @@ const handleModalSubmission = async (interaction) => {
 
   const channel = await interaction.client.channels.fetch(userData.channelId)
 
+  // Find the most recent message in the channel
+  const messages = await channel.messages.fetch({ limit: 1 })
+  const latestMessage = messages.first()
+
   await interaction.reply({
     content: `Your ${questionId} has been ${
       userData.isNewResponse ? 'recorded' : 'updated'
     } to: "${response}"`,
     ephemeral: true,
   })
+
+  // Specifically check if this question is NOT required
+  if (question && !question.required) {
+    // Ensure the message has components and they're not empty
+    if (latestMessage.components && latestMessage.components.length > 0) {
+      // Create a new action row without the skip button
+      const updatedComponents = latestMessage.components[0].components.filter(
+        (component) => !component.customId.startsWith('skip-')
+      )
+
+      // If there are still components, create a new ActionRow
+      if (updatedComponents.length > 0) {
+        await latestMessage.edit({
+          components: [
+            new ActionRowBuilder().addComponents(...updatedComponents),
+          ],
+        })
+      } else {
+        // If no components remain, clear them completely
+        await latestMessage.edit({ components: [] })
+      }
+    }
+  }
 
   if (userData.hasUpdatedResponse && userData.isComplete()) {
     await showSummary(channel, userData)
--- src/events/interactions/handleSelectMenu.js
+++ src/events/interactions/handleSelectMenu.js
@@ -1,12 +1,13 @@
 const { showSummary, sendNextQuestion } = require('../../utils/questionHandler')
 const UserManager = require('../../utils/UserManager')
+const { ActionRowBuilder } = require('discord.js')
 const { questions } = require('../../questions')
 
 const handleSelectMenu = async (interaction) => {
-  // Remove both '-select' and '-update' from the ID to get the base questionId
   const questionId = interaction.customId
     .replace(/-select$/, '')
     .replace(/-update$/, '')
+
   const values = interaction.values
   const question = questions.find((q) => q.id === questionId)
 
@@ -29,6 +30,31 @@ const handleSelectMenu = async (interaction) => {
   })
 
   const channel = await interaction.client.channels.fetch(userData.channelId)
+  const messages = await channel.messages.fetch({ limit: 1 })
+  const latestMessage = messages.first()
+
+  // Specifically check if this question is NOT required
+  if (question && !question.required) {
+    // Ensure the message has components and they're not empty
+    if (latestMessage.components && latestMessage.components.length > 0) {
+      // Create a new action row without the skip button
+      const updatedComponents = latestMessage.components[0].components.filter(
+        (component) => !component.customId.startsWith('skip-')
+      )
+
+      // If there are still components, create a new ActionRow
+      if (updatedComponents.length > 0) {
+        await latestMessage.edit({
+          components: [
+            new ActionRowBuilder().addComponents(...updatedComponents),
+          ],
+        })
+      } else {
+        // If no components remain, clear them completely
+        await latestMessage.edit({ components: [] })
+      }
+    }
+  }
 
   if (userData.hasUpdatedResponse && userData.isComplete()) {
     await showSummary(channel, userData)
