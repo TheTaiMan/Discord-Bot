1b899cb286b2c9f11b16c32e173f333776a5a84f
TheTaiMan <12tayenm2@gmail.com>
2024-11-29 11:28:14 -0600

    fixed

--- src/components/createUpdateComponents.js
+++ src/components/createUpdateComponents.js
@@ -1,12 +1,20 @@
+const { ActionRowBuilder } = require('discord.js')
 const createModalButton = require('./createModalButton')
 const createUpdateSelectMenu = require('./createUpdateSelectMenu')
 
 const createUpdateComponents = (questions) => {
   return questions.map((question) => {
+    const row = new ActionRowBuilder()
+
     if (question.type === 'modal') {
-      return createModalButton(question, false)
+      const button = createModalButton(question, false)
+      row.addComponents(button.components[0])
+    } else {
+      const selectMenu = createUpdateSelectMenu(question)
+      row.addComponents(selectMenu.components[0])
     }
-    return createUpdateSelectMenu(question)
+
+    return row
   })
 }
 
