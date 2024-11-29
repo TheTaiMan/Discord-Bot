c0058295a23361e2c3da4c536d22ffc5dd735011
TheTaiMan <12tayenm2@gmail.com>
2024-11-29 10:09:23 -0600

    Made it clean

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,9 +1,9 @@
 const handleOnboarding = require('../interactions/handleOnboarding')
 const handleModal = require('../interactions/handleModal')
-const handleSubmit = require('../interactions/handleSubmit')
 const handleSkipButton = require('../interactions/handleSkipButton')
-const handleModalSubmission = require('../interactions/handleModalSubmission')
+const handleSubmit = require('../interactions/handleSubmit')
 
+const handleModalSubmission = require('../interactions/handleModalSubmission')
 const handleSelectMenu = require('../interactions/handleSelectMenu')
 
 module.exports = {
@@ -11,17 +11,16 @@ module.exports = {
   async execute(interaction) {
     // Handling Button Interactions
     if (interaction.isButton()) {
-      if (interaction.customId === 'submit-form') {
-        await handleSubmit(interaction)
-      } else if (interaction.customId === 'start-onboarding') {
+      if (interaction.customId === 'start-onboarding') {
         await handleOnboarding(interaction)
-      } else if (interaction.customId.startsWith('skip-')) {
-        await handleSkipButton(interaction)
       } else if (interaction.customId.endsWith('-question')) {
         await handleModal(interaction)
+      } else if (interaction.customId.startsWith('skip-')) {
+        await handleSkipButton(interaction)
+      } else if (interaction.customId === 'submit-form') {
+        await handleSubmit(interaction)
       }
     }
-
     // Handling model submit button
     if (interaction.isModalSubmit()) {
       await handleModalSubmission(interaction)
